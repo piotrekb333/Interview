f2f0a5c52085a17ee0791d7c7a1af63646716270
Piotr Bancarzewski <piotr.bancarzewski3@gmail.com>
2019-06-30 23:29:03 +0200

    added seed

--- InterviewLists/InterviewLists.Persistence/InterviewInitializer.cs
+++ InterviewLists/InterviewLists.Persistence/InterviewInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InterviewLists.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -16,6 +17,39 @@ namespace InterviewLists.Persistence
         public void SeedEverything(InterviewDbContext context)
         {
             context.Database.EnsureCreated();
+
+            SeedCarMakes(context);
+            SeedCarModels(context);
+        }
+        public void SeedCarMakes(InterviewDbContext context)
+        {
+            var makes = new[]
+            {
+               new CarMake {Title="BMW" },
+               new CarMake {Title="Audi" },
+               new CarMake {Title="Volvo" }
+            };
+            context.CarMakes.AddRange(makes);
+        }
+
+        public void SeedCarModels(InterviewDbContext context)
+        {
+            var models = new[]
+            {
+               new CarModel {Title="A4",CarMakeId=1 },
+               new CarModel {Title="A5",CarMakeId=1 },
+               new CarModel {Title="A6",CarMakeId=1 },
+
+               new CarModel {Title="M3",CarMakeId=2 },
+               new CarModel {Title="M4",CarMakeId=2 },
+               new CarModel {Title="M5",CarMakeId=2 },
+
+               new CarModel {Title="S70",CarMakeId=3 },
+               new CarModel {Title="S80",CarMakeId=3 },
+               new CarModel {Title="S90",CarMakeId=3 },
+            };
+            context.CarModels.AddRange(models);
+
         }
     }
 }
